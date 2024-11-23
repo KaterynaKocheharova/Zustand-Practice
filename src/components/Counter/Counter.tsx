dba3bbbe83c11bd9855cf47e22fae1bf2aabd23c
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-23 15:51:15 +0300

    expore different ways to get state

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,6 +1,23 @@
 import { useCounterStore } from "../../store";
 
+// USING USE COUNTER STORE OUTSIDE THE COMPONENT
+
+const logCount = () => {
+  console.log(useCounterStore)
+  const count = useCounterStore.getState().count;
+  console.log(count);
+};
+
+logCount()
+
 const Counter = () => {
+  // another way to access the store
+  // hook returns the store when invoked
+  // returns all store object at once, probably not good for performance
+  // const { increment, decrement, incrementAsync, count } = useCounterStore();
+
+  // returns only a part of the store, component will rerender only if this part changes, so
+  // it's better for optimization
   const count = useCounterStore((store) => store.count);
   const increment = useCounterStore((store) => store.increment);
   const decrement = useCounterStore((store) => store.decrement);
