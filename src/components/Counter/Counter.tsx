9feae70e7d0f67ad093afa81dd5989c37ec766c2
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-23 15:32:25 +0300

    learn to add async functions to store

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -4,10 +4,13 @@ const Counter = () => {
   const count = useCounterStore((store) => store.count);
   const increment = useCounterStore((store) => store.increment);
   const decrement = useCounterStore((store) => store.decrement);
+  const incrementAsync = useCounterStore((store) => store.incrementAsync);
+
   return (
     <div>
       <p>{count}</p>
       <button onClick={increment}>Increment</button>
+      <button onClick={incrementAsync}>Increment Async</button>
       <button onClick={decrement}>Decrement</button>
     </div>
   );
--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,7 @@ type CounterStore = {
   count: number;
   increment: () => void;
   decrement: () => void;
+  incrementAsync: () => Promise<void>;
 };
 
 export const useCounterStore = create<CounterStore>((set) => ({
@@ -16,6 +17,14 @@ export const useCounterStore = create<CounterStore>((set) => ({
   decrement: () => {
     set((state) => ({
       count: state.count - 1,
+    }));
+  },
+  incrementAsync: async () => {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+    set((state) => ({
+      count: state.count + 1,
     }));
   },
 }));
